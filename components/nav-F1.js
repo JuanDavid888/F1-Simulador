24f2042281b5ba7becb261bf04d1d69a2d699ed8
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-04-28 17:31:16 -0500

    feat: :lipstick: Se agregó la barra de categorias

---
+++ components/nav-F1.js
@@ -0,0 +1,79 @@
+class NavF1 extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: "open" });
+
+        // Crear el estilo
+        const style = document.createElement("style");
+        style.innerHTML = `
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 1rem;
+        }
+
+        .nav-f1 {
+            display: flex;
+            overflow-x: auto;
+        }
+
+        ul {
+            display: flex;
+            gap: 1rem;
+            justify-content: center;
+            align-items: center;
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+        
+        .boton-nav {
+            width: 8rem;
+            height: 2.5rem;
+            border: none;
+            border-radius: 0.5rem;
+            color: #000;
+            font-size: 1rem;
+            font-weight: bold;
+            cursor: pointer;
+        }
+
+        .boton-nav:hover {
+            transform: scale(1.1);
+            background-color: #ff3737;
+            color: #fff;
+            transition: transform 0.2s ease-in-out;
+        }
+
+        @media screen and (min-width: 700px) {
+            .nav-f1 {
+                display: flex;
+                overflow-x: auto;
+                justify-content: center;
+            }
+        }
+        `;
+
+        // Crear la estructura HTML
+        const nav = document.createElement("nav");
+        nav.classList.add("nav-f1");
+        nav.innerHTML = `
+            <div class="container">
+                <ul>
+                    <li><button class="boton-nav">Pilotos</button></li>
+                    <li><button class="boton-nav">Equipos</button></li>
+                    <li><button class="boton-nav">Circuitos</button></li>
+                    <li><button class="boton-nav">Vehículos</button></li>
+                </ul>
+            </div>
+        `;
+
+        // Agregar el estilo y el nav al shadow DOM
+        shadow.appendChild(style);
+        shadow.appendChild(nav);
+    }
+}
+
+customElements.define("nav-f1", NavF1);
+export { NavF1 };
--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
 import '../components/content-F1.js'
+import '../components/nav-F1.js'
